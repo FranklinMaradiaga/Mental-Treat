0d88aaf3589c8c426645665e96c78aa72ed97bb8
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-29 16:26:25 +0000

    updated app.py

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask, render template
+from flask import Flask, render_template
 
 # Create a Flask instance
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return render template('home.html')
-    
-@app.route('/login', methods= ['GET', 'POST'])
+    return render_template('index.html')
+
+@app.route('/login')
 def login():
-    form = LoginForm()
-    if form.validate_on_submit(): #checks if entries are valid 
-    user = User(username= form.username.data, email=form.email.data, password=form.password.data)
-    db.session.add(user)
-    db.session.commit()
-    flash(f' Account created for {form.username.data}!, 'success')
-    return redirect(url_for('home')) #if so-send to home page
-    return render_template('login.html', title= 'Login', form=form)
+    return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+    
+# @app.route('/login', methods= ['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit(): #checks if entries are valid 
+#     user = User(username= form.username.data, email=form.email.data, password=form.password.data)
+#     db.session.add(user)
+#     db.session.commit()
+#     flash(f' Account created for {form.username.data}!, 'success')
+#     return redirect(url_for('home')) #if so-send to home page
+#     return render_template('login.html', title= 'Login', form=form)
 
 
     
