b2fce4470bdfc4c5286d391b90a36280078a95ac
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-30 21:38:31 +0000

    updated files

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask, render_template, request
-from registration import RegistrationForm
 import secrets
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create a Flask instance
 app = Flask(__name__)
+# Secret Key
 app.config['SECRET_KEY'] = secrets.token_hex(16)
+# Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+#Initialize the Database
+db = SQLAlchemy(app)
+
+# Create Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create A String
+    def __repr__(self):
+        return '<Name %r>' % self.email
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,33 +36,29 @@ def login():
     if request.method == "POST":
         email = request.form.get("myemail")
         password = request.form.get("password")
-        if request.form.get("button") == "Clicked":
-            print("You know it")
-            return meditation()
 
+        if request.form.get("button") == "Clicked":
+            user = Users.query.filter_by(email=email).first()
+
+            if user is None:
+                user = Users(email=email)
+                db.session.add(user)
+                db.session.commit()
+            
+                flash("You know it", user)
+            return meditation()
+        our_users = Users.query.order_by(Users.date_added)
     return render_template('signin.html')
+
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
+
 @app.route('/meditation')
 def meditation():
     return render_template('meditation.html')
-
-
-    
-# @app.route('/login', methods= ['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit(): #checks if entries are valid 
-#     user = User(username= form.username.data, email=form.email.data, password=form.password.data)
-#     db.session.add(user)
-#     db.session.commit()
-#     flash(f' Account created for {form.username.data}!, 'success')
-#     return redirect(url_for('home')) #if so-send to home page
-#     return render_template('login.html', title= 'Login', form=form)
-
 
     
 if __name__ == '__main__':
